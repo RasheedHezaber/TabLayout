2c76fa53d8dba676b7b39e25b8bc80fe25cd57aa
rasheed hezaber <rasheedhezaber@gmail.com>
2020-11-04 01:14:57 -0800

    one fragment

--- app/src/main/java/com/example/tablayout/FirstFragment.kt
+++ app/src/main/java/com/example/tablayout/FirstFragment.kt
@@ -5,12 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
+private lateinit var tabfragment: TextView
 /**
  * A simple [Fragment] subclass.
  * Use the [FirstFragment.newInstance] factory method to
@@ -34,20 +35,30 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+        val view= inflater.inflate(R.layout.fragment_first, container, false)
+        tabfragment= view.findViewById(R.id.tab_fragment) as TextView
+        if (param1=="0"){
+            view.setBackgroundColor(resources.getColor(R.color.color1))
+            tabfragment.setText("This is fragment 1")
+        }
 
+        if(param1=="1")
+        {
+            view.setBackgroundColor(resources.getColor(R.color.color2))
+            tabfragment.setText("This is fragment 2")
+        }
+
+        if(param1=="2")
+        {
+            view.setBackgroundColor(resources.getColor(R.color.color3))
+            tabfragment.setText("This is fragment 3")
+        }
+
+
+        return view
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FirstFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
         fun newInstance(param1: String, param2: String) =
             FirstFragment().apply {
                 arguments = Bundle().apply {
@@ -57,3 +68,5 @@ class FirstFragment : Fragment() {
             }
     }
 }
+
+
--- app/src/main/java/com/example/tablayout/MainActivity.kt
+++ app/src/main/java/com/example/tablayout/MainActivity.kt
@@ -1,19 +1,25 @@
  package com.example.tablayout
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.example.tablayout.SecondFragment.Companion.newInstance
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.reflect.Array.newInstance
 
  class MainActivity : AppCompatActivity() {
      lateinit var tabLayaut: TabLayout
      lateinit var tabViewPager: ViewPager2
+     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
      override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
          setContentView(R.layout.activity_main)
@@ -26,9 +32,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
              override fun createFragment(position: Int): Fragment {
                  return when (position) {
-                     0 -> FirstFragment.newInstance("", "")
-                     1 -> SecondFragment.newInstance("", "")
-                     2 -> ThirdFragment.newInstance("", "")
+                     0 -> FirstFragment.newInstance(position.toString(),"")
+                     1 -> FirstFragment.newInstance(position.toString(),"")
+                     2 -> FirstFragment.newInstance(position.toString(),"")
+
                      else -> {
                          SecondFragment.newInstance("", "")
                      }
@@ -50,15 +57,24 @@ import kotlinx.android.synthetic.main.activity_main.*
                  2 -> "tab3"
                  else -> null
              }
+             tab.icon = when(position){
+                 0 -> getDrawable(R.drawable.ic_baseline_mood_24)
+                 1 -> getDrawable(R.drawable.ic_baseline_mood_bad_24)
+                 2 -> getDrawable(R.drawable.ic_baseline_mood_24)
+                 else -> null
+             }
          }.attach()
-         icon()
-
-     }
-
-     private fun icon() {
-         tabLayaut.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_mood_24)
+       /*  tabLayaut.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_mood_24)
          tabLayaut.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_mood_bad_24)
-         tabLayaut.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_mood_24)
-     }
+         tabLayaut.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_mood_24)*/
+
+             tabViewPager.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
+                 override fun onPageSelected(position: Int) {
+                     super.onPageSelected(position)
+                     Toast.makeText(this@MainActivity,"page ${position}",Toast.LENGTH_LONG).show()
+                 }
+             })
+         }
+
 
  }
